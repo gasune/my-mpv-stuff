7da95460ba4edae6191b7756b82ab23483b223a1
trashgasu <86105239+trashgasu@users.noreply.github.com>
2022-07-31 09:38:37 +0800

    Add files via upload

--- scripts/visualizer.lua
+++ scripts/visualizer.lua
@@ -136,7 +136,7 @@ options.read_options(opts)
 opts.height = math.min(12, math.max(4, opts.height))
 opts.height = math.floor(opts.height)
 
-local function get_visualizer(name, quality)
+local function get_visualizer(name, quality, vtrack)
     local w, h, fps
 
     if quality == "verylow" then
@@ -255,26 +255,41 @@ local function get_visualizer(name, quality)
                 "mode           = p2p," ..
             "format             = rgb0 [vo]"
     elseif name == "off" then
-        return "[aid1] afifo [ao]"
+        local hasvideo = false
+        for id, track in ipairs(mp.get_property_native("track-list")) do
+            if track.type == "video" then
+                hasvideo = true
+                break
+            end
+        end
+        if hasvideo then
+            return "[aid1] asetpts=PTS [ao]; [vid1] setpts=PTS [vo]"
+        else
+            return "[aid1] asetpts=PTS [ao];" ..
+                "color      =" ..
+                    "c      = Black:" ..
+                    "s      =" .. w .. "x" .. h .. "," ..
+                "format     = yuv420p [vo]"
+        end
     end
 
     msg.log("error", "invalid visualizer name")
     return ""
 end
 
-local function select_visualizer(atrack, vtrack, albumart)
+local function select_visualizer(vtrack)
     if opts.mode == "off" then
         return ""
     elseif opts.mode == "force" then
-        return get_visualizer(opts.name, opts.quality)
+        return get_visualizer(opts.name, opts.quality, vtrack)
     elseif opts.mode == "noalbumart" then
-        if albumart == 0 and vtrack == 0 then
-            return get_visualizer(opts.name, opts.quality)
+        if vtrack == nil or vtrack.albumart then
+            return get_visualizer(opts.name, opts.quality, vtrack)
         end
         return ""
     elseif opts.mode == "novideo" then
-        if vtrack == 0 then
-            return get_visualizer(opts.name, opts.quality)
+        if vtrack == nil then
+            return get_visualizer(opts.name, opts.quality, vtrack)
         end
         return ""
     end
@@ -283,32 +298,40 @@ local function select_visualizer(atrack, vtrack, albumart)
     return ""
 end
 
+local current_visualizer = ""
 local function visualizer_hook()
     local count = mp.get_property_number("track-list/count", -1)
-    local atrack = 0
-    local vtrack = 0
-    local albumart = 0
     if count <= 0 then
         return
     end
-    for tr = 0,count-1 do
-        if mp.get_property("track-list/" .. tr .. "/type") == "audio" then
-            atrack = atrack + 1
-        else
-            if mp.get_property("track-list/" .. tr .. "/type") == "video" then
-                if mp.get_property("track-list/" .. tr .. "/albumart") == "yes" then
-                    albumart = albumart + 1
-                else
-                    vtrack = vtrack + 1
-                end
+
+    local atrack = mp.get_property_native("current-tracks/audio")
+    local vtrack = mp.get_property_native("current-tracks/video")
+
+    --no tracks selected (yet)
+    if atrack == nil and vtrack == nil then
+        for id, track in ipairs(mp.get_property_native("track-list")) do
+            if track.type == "video" and (vtrack == nil or vtrack.albumart == true) and mp.get_property("vid") ~= "no" then
+                vtrack = track
+            elseif track.type == "audio" then
+                atrack = track
             end
         end
     end
 
-    mp.set_property("options/lavfi-complex", select_visualizer(atrack, vtrack, albumart))
+    --prevent endless loop
+    if current_visualizer ~= opts.name then
+        local lavfi = select_visualizer(vtrack)
+        if lavfi ~= "" then
+            current_visualizer = opts.name
+        end
+        mp.set_property("options/lavfi-complex", lavfi)
+    end
 end
 
 mp.add_hook("on_preloaded", 50, visualizer_hook)
+mp.observe_property("current-tracks/audio", "native", visualizer_hook)
+mp.observe_property("current-tracks/video", "native", visualizer_hook)
 
 local function cycle_visualizer()
     local i, index = 1
